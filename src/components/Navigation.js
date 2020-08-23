2e84e50371d49b75315088fe6223935983702bc4
J Perrin <perrinjack96@gmail.com>
2020-08-23 14:04:56 +0100

    ActiveclassName added to each Nav-Link - to be used later with styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,17 +5,29 @@ const Navigation = () => {
   return (
     <ul className="nav justify-content-center fixed-top">
       <li className="nav-item">
-        <NavLink className="nav-link" to="/australia">
+        <NavLink
+          activeClassName="is-active"
+          className="nav-link"
+          to="/australia"
+        >
           Australia
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink className="nav-link" to="/australia">
+        <NavLink
+          activeClassName="is-active"
+          className="nav-link"
+          to="/australia"
+        >
           New Zealand
         </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink className="nav-link" to="/australia">
+        <NavLink
+          activeClassName="is-active"
+          className="nav-link"
+          to="/australia"
+        >
           U.K
         </NavLink>
       </li>
