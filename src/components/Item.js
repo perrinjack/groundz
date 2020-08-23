79522e029040fb65ecb6beb1074433e720656d40
J Perrin <perrinjack96@gmail.com>
2020-08-23 17:18:37 +0100

    Spiked calling of api on mounting of Item Component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import apiKey from '../config.js';
+import { apiKey } from '../config.js';
+import axios from 'axios';
 
 export default class Item extends React.Component {
   constructor(props) {
@@ -8,6 +9,27 @@ export default class Item extends React.Component {
       location: props.location,
       apiResponse: null,
     };
+  }
+
+  async searchImages() {
+    axios
+      .get(
+        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=australia&per_page=3&format=json&nojsoncallback=1`
+      )
+      .then((response) => {
+        this.setState({ apiResponse: response.data });
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(
+          'Encountered an error with fetching images from Flickr',
+          error
+        );
+      });
+  }
+
+  componentDidMount() {
+    this.searchImages();
   }
 
   render() {
