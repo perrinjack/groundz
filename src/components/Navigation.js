76e0b69cbd440acd5e823bb664d0cd21fc551f2a
J Perrin <perrinjack96@gmail.com>
2020-08-23 13:59:42 +0100

    Added Nav-link class to each nav link item

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,13 +5,19 @@ const Navigation = () => {
   return (
     <ul className="nav justify-content-center fixed-top">
       <li className="nav-item">
-        <NavLink to="/australia">Australia</NavLink>
+        <NavLink className="nav-link" to="/australia">
+          Australia
+        </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink to="/australia">New Zealand</NavLink>
+        <NavLink className="nav-link" to="/australia">
+          New Zealand
+        </NavLink>
       </li>
       <li className="nav-item">
-        <NavLink to="/australia">U.K</NavLink>
+        <NavLink className="nav-link" to="/australia">
+          U.K
+        </NavLink>
       </li>
     </ul>
   );
