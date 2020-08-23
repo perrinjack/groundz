de43b180036488a9365e2698c9e65fcbc1da7684
J Perrin <perrinjack96@gmail.com>
2020-08-23 14:51:14 +0100

    App renders without crashing test passed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer class="page-footer font-small cyan darken-3">
+    <footer className="page-footer font-small cyan darken-3">
       <br></br>
-      <div class="footer-copyright text-center py-3">
+      <div className="footer-copyright text-center py-3">
         <a href="https://github.com/perrinjack">
           {' '}
-          <i class="fa fa-github fa-2x" aria-hidden="true"></i>{' '}
+          <i className="fa fa-github fa-2x" aria-hidden="true"></i>{' '}
         </a>
         <a href="https://www.linkedin.com/in/perrinjack/">
           {' '}
-          <i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+          <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
         </a>
         <br></br>© 2020 Copyright: J Perrin
       </div>
