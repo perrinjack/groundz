fd03ef8ff2a1ea1e0d5ab0dd4536373f67609cf6
J Perrin <perrinjack96@gmail.com>
2020-08-24 11:58:57 +0100

    RED Image component renders without crashing

---
+++ src/Image.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import Image from './components/Image';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Image />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
