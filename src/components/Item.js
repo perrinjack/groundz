ad2018496ef7c8f313ac4e91d9db40e74e27c91b
J Perrin <perrinjack96@gmail.com>
2020-08-24 11:14:53 +0100

    Image componenent stock image rendered on receiving of apiResponse

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { apiKey } from '../config.js';
 import axios from 'axios';
-
+import Gallery from './Gallery.js'
 export default class Item extends React.Component {
   constructor(props) {
     super(props);
@@ -14,14 +14,10 @@ export default class Item extends React.Component {
   async searchImages() {
     axios
       .get(
-        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${this.state.location}&per_page=3&format=json&nojsoncallback=1`
+        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${this.state.location}&per_page=1&format=json&nojsoncallback=1`
       )
       .then((response) => {
-
-        
-        
-        this.setState({ apiResponse: 'test' });
-        
+        this.setState({ apiResponse: <Gallery /> });
       })
       .catch((error) => {
         console.log(
@@ -36,6 +32,6 @@ export default class Item extends React.Component {
   }
 
   render() {
-    return <h1>hello</h1>;
+  return <h1>{this.state.apiResponse}</h1>;
   }
 }
