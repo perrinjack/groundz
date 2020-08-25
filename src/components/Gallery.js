341cce6c5587acf6edf8f7708a3d8d4020a09628
J Perrin <perrinjack96@gmail.com>
2020-08-25 16:36:36 +0100

    Flickr Image description is displayed on card

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Image from './Image.js';
 const Gallery = (props) => {
-  console.log(props.data);
   const results = props.data;
   let images;
 
@@ -12,7 +11,8 @@ const Gallery = (props) => {
     let secret = image.originalsecret;
     let title = image.title;
     let url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
-    return <Image url={url} title={title} />;
+    let description = image.description._content;
+    return <Image url={url} title={title} description={description} />;
   });
 
   return (
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const Image = ({ url, title }) => (
+const Image = ({ url, title , description}) => (
   <div className="col-12 col-md-6 col-lg-4 mb-4">
     <div className="card">
-      <img className="card-img-top" src={url} alt={title} />
+      <img className="card-img-top" src={url} />
       <div className="card-body">
-        <h6 className="card-subtitle mb-2 text-right font-italic">
-          {title}
-        </h6>
+        <h6 className="card-subtitle mb-2 text-right font-italic">{title}</h6>
+        {description}
       </div>
     </div>
   </div>
