9442e5ad75d2a60a941137117f1de1e3578aa70c
J Perrin <perrinjack96@gmail.com>
2020-08-25 17:01:19 +0100

    Moment package installed- to alter date taken into readable format

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Image from './Image.js';
+import Moment from 'moment';
+
 const Gallery = (props) => {
   const results = props.data;
   let images;
@@ -12,7 +14,7 @@ const Gallery = (props) => {
     let title = image.title;
     let url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
     let description = image.description._content;
-    let date_taken = image.datetaken;
+    let date_taken = Moment(image.datetaken).format('MMM Do YYYY')
     return (
       <Image
         url={url}
